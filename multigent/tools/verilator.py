0d70402dd455f648e127a8c8d08404f2b65d06c4
Manvi Jha <drprofmjha@gmail.com>
2026-09-02 17:20:43 -0500

    Add Verilator tool scaffold

---
+++ multigent/tools/verilator.py
@@ -0,0 +1,5 @@
+"""Verilator lint/compile/simulation wrapper.
+
+This node will execute Verilator deterministically and return structured compiler,
+elaboration, warning, and simulation status to LangGraph.
+"""
